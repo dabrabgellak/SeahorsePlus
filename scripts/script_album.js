9b361772d5d7f59a6e31ac9bc591539cbf660375
Caroll1889 <diahancaroll@hotmail.com>
2020-03-17 17:21:27 -0500

    album script

---
+++ scripts/script_album.js
@@ -0,0 +1,28 @@
+const params = window.location.search.substr(1);
+artist, album = params.split('&');
+
+const req_url = 'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=a8c031c3b6f2b093bd851d1e525234d2&artist='+artist+'&album='+album+'&format=json';
+$.ajax({
+    url: req_url,
+    type: 'GET',
+    success: function (data) {
+        print_data_track(data);
+    },
+    error: function (data) {
+        console.log(data);
+    }
+});
+
+function print_data_track(data){
+    for (const i in data.tracks.track){
+        const track = data.tracks.track[i];
+        const struct = [
+        '<h3>',
+        track.name,
+        '</h3>',
+        ];
+        console.log(struct.join(''));
+        console.log($('.Tracks'));
+        $('.Tracks').append($(struct.join('')));
+       }
+    }
