c426779b6648847d928fa6899865019336d56225
dalexach <dalexach@gmail.com>
2020-03-20 15:34:20 -0500

    Adding some youtube urls

--- scripts/script_album.js
+++ scripts/script_album.js
@@ -2,7 +2,9 @@ const params = window.location.search.substr(1);
 let all = params.split("/"); 
 let artist = all[0].replace("+","%20");
 let album = all[1].replace("+","%20");
-console.log(artist);
+let urltracks = [];
+let ytracks = [];
+
 
 const req_url = 'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=a8c031c3b6f2b093bd851d1e525234d2&artist='+artist+'&album='+album+'&format=json';
 $.ajax({
@@ -22,10 +24,18 @@ function print_data_track(data){
         const struct = [
         '<h3>',
         track.name,
-        '</h3>',
+        '</h3>'
         ];
-        console.log(struct.join(''));
-        console.log($('.Tracks'));
+        urltracks.push(track.url);
         $('.Tracks').append($(struct.join('')));
        }
+       console.log(urltracks);
+       get_songs();
+    }
+    
+function get_songs(){
+    for (const i in urltracks){
+        console.log(urltracks[i]);
+    }
+    
     }
